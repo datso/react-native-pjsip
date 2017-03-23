4ce36661416dfbf9917fec47454dc36c7006f428
vadim.ruban <vadim.ruban@carusto.com>
2017-03-23 17:58:49 +0200

    Remove video view as it cause error on Androi

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import Account from './src/Account';
 import Call from './src/Call';
 import Endpoint from './src/Endpoint';
-import VideoView from './src/VideoView';
+// import VideoView from './src/VideoView';
 
 module.exports = {
     Account,
     Call,
     Endpoint,
-    VideoView
+    // VideoView
 }
