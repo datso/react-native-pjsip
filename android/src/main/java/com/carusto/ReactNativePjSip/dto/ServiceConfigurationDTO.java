f6343942d3b9e9753c6aaf102793d354577cc716
Chris Mendes <cmendes0101@gmail.com>
2018-02-08 18:46:51 -0600

    Adding stun configuration for android

--- android/src/main/java/com/carusto/ReactNativePjSip/dto/ServiceConfigurationDTO.java
+++ android/src/main/java/com/carusto/ReactNativePjSip/dto/ServiceConfigurationDTO.java
@@ -4,19 +4,34 @@ import android.content.Intent;
 
 import com.facebook.react.bridge.ReadableMap;
 import org.json.JSONObject;
+import org.pjsip.pjsua2.StringVector;
 
 import java.util.Map;
+import java.util.ArrayList;
 
 public class ServiceConfigurationDTO {
 
     public String ua;
+    public ArrayList<String> stun;
 
     public String getUserAgent() {
         return ua;
     }
 
+    public StringVector getStunServers() {
+        StringVector serversVector = new StringVector();
+        for (String server : stun) {
+            serversVector.add(server);
+        }
+        return serversVector;
+    }
+
     public boolean isUserAgentNotEmpty() {
         return ua != null && !ua.isEmpty();
+    }
+
+    public boolean isStunServersNotEmpty() {
+        return stun != null && stun.size() > 0;
     }
 
     public JSONObject toJson() {
@@ -46,6 +61,10 @@ public class ServiceConfigurationDTO {
 
         if (conf.containsKey("ua")) {
             c.ua = conf.get("ua").toString();
+        }
+
+        if (conf.containsKey("stun")) {
+            c.stun = (ArrayList) conf.get("stun");
         }
 
         return c;
