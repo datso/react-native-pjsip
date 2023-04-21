bdd112bd1307851b24b8a3cf65ce3fa58f9b0984
Aniruddh Kukadiya <kukadiya.ani@gmail.com>
2023-04-21 10:21:03 +0530

    update version and added JNI lib files

--- libs.sh
+++ libs.sh
@@ -10,6 +10,6 @@ if ! type "tar" > /dev/null; then
     exit 1; 
 fi
 
-curl -LO https://github.com/datso/react-native-pjsip-builder/releases/download/v2.7.1-with-vialer/release.tar.gz
+curl -LO https://github.com/kukadiyaAni/react-native-pjsip/releases/download/V3.0.0/release.zip
 tar -xvf release.tar.gz
 rm release.tar.gz
